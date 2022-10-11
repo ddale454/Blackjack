2214b7773a6287209a04488240394bdfd9464afe
Dylan Dale <ddale454@student.fuhsd.org>
2022-10-10 22:10:09 -0700

    Add files via upload

--- Blackjack.py
+++ Blackjack.py
@@ -1,10 +1,10 @@
 import pygame, random
 
 pygame.init()
-screen = pygame.display.set_mode([1200, 600])
+screen = pygame.display.set_mode([1800, 900])
 screen.fill([255,255,255])
 x = 100
-y = 100
+y = 500
 
 
 _RANK_STRINGS = (
@@ -12,6 +12,10 @@ _RANK_STRINGS = (
 )
 
 _SUITS = ["hearts", "diamonds", "spades", "clubs"]
+
+_CARD_WIDTH = 150
+_CARD_HEIGHT = 300
+_CARD_SPACING = 10
 
 class Card:
     rank: int
@@ -24,35 +28,52 @@ class Card:
     def __str__(self) -> str:
         return _RANK_STRINGS[self.rank - 1] + ' of ' + self.suit
 
+    def draw(self, screen, x, y):
+        pygame.draw.rect(screen, [0, 0, 0], pygame.Rect(x, y, _CARD_WIDTH, _CARD_HEIGHT), 1)
+        name_font = pygame.font.Font(None, 25)
+        name_render = name_font.render(str(self), 1, (0, 0, 0))
+        name_pos = [x + 10, y + 10]
+        screen.blit(name_render, name_pos);
+
+class Hand:
+    def __init__(self):
+        self.cards = []
+
+    def add_card(self, card: Card):
+        self.cards.append(card)
+
+    def value(self):
+        return 0
+
+    def draw(self, screen, x, y):
+        for card in self.cards:
+            card.draw(screen, x, y)
+            x += _CARD_WIDTH + _CARD_SPACING
+
 deck = []
 for card in range(1, 14):
     for suit in _SUITS:
         deck.append(Card(card, suit))
         
 random.shuffle(deck)
-hand = []
+hand = Hand()
 for deal in range(2):
-    hand.append(deck.pop(0))
+    hand.add_card(deck.pop(0))
 
-   
+hand.draw(screen, x, y)
 
-for card in hand:
-    pygame.draw.rect(screen, [0,0,0], pygame.Rect(x, y, 300, 500), 1)
-    name_font = pygame.font.Font(None, 25)
-    name_render = name_font.render(str(card), 1, (0,0,0))
-    name_pos = [x + 10, y + 10]
-    screen.blit(name_render, name_pos);
-    x += 310
 pygame.display.flip()
 
 running = True
 while running:
-  
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
         elif event.type == pygame.KEYDOWN:
             if event.key == pygame.K_TAB:
-                hand.append(deck.pop(0))
+                hand.add_card(deck.pop(0))
+                screen.fill((255, 255, 255))
+                hand.draw(screen, x, y)
+                pygame.display.flip()
 
 pygame.quit
